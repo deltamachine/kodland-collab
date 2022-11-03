57f31b035ef91a3f0cbda5c079e07d28d08ff4b4
Анна Кондратьева <an-an-kondratjeva@yandex.ru>
2022-11-04 00:10:21 +0300

    add all needed files

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/channel/:student_id/:task_id/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.BroadcastFilter(msg, func(q *melody.Session) bool {
+			return q.Request.URL.Path == s.Request.URL.Path
+		})
+	})
+
+	r.Run(":8888")
+}
